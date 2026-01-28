724dca51aa066c299a59a361a15045404ae2001e
Noah <82201000+Nopro263@users.noreply.github.com>
2026-01-28 20:48:22 +0100

    functional shutdown

--- code/deployment/start.sh
+++ code/deployment/start.sh
@@ -6,4 +6,5 @@ cd /app
 
 nginx
 
-cd /app/backend && fastapi run main.py --host 127.0.0.1 --port 8000 --forwarded-allow-ips="127.0.0.1" --root-path /api
+cd /app/backend
+exec fastapi run main.py --host 127.0.0.1 --port 8000 --forwarded-allow-ips="127.0.0.1" --root-path /api
