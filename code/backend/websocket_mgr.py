18479b65bb0f5cc76acbfee43f7d7321bdf5fc3d
timk20071 <tim.kainhofer@gmx.at>
2026-02-25 08:19:31 +0100

    resolved merge confilicts

--- code/backend/websocket_mgr.py
+++ code/backend/websocket_mgr.py
@@ -1,7 +1,12 @@
+import threading
+from threading import Thread
 from starlette.websockets import WebSocket, WebSocketDisconnect
-from websocket.ws_messages import messages, ConnectToDrone, Land, TakeOff, FunkiMessage, ClientBoundMessage, DroneConnected, DroneDisconnected
+from code.backend.dronemaster.connection import PathCalculation, CanvasWaypoints
+from dronemaster import Drone, State
+from websocket.ws_messages import messages, ConnectToDrone, Land, TakeOff, FunkiMessage, ClientBoundMessage, DroneConnected, DroneDisconnected, DisconnectFromDrone, Error, Accepted, StateMessage
 from dronemaster import Drone
 from database import SessionLocal
+
 
 class WebsocketManager:
     def __init__(self) -> None:
@@ -10,9 +15,19 @@ class WebsocketManager:
     async def connnect(self, ws: WebSocket):
         self.connections[ws] = WsConnection(ws, self)
     
+    async def disconnect(self, ws: WebSocket):
+        await self.connections[ws].disconnect("WS disconnect")
+        del self.connections[ws]
+    
     async def send(self, ws: WebSocket, data: ClientBoundMessage):
         try:
             await ws.send_json(data.model_dump())
+        except WebSocketDisconnect:
+            del self.connections[ws]
+
+    async def send_bytes(self, ws: WebSocket, data: bytes):
+        try:
+            await ws.send_bytes(data)
         except WebSocketDisconnect:
             del self.connections[ws]
     
@@ -23,29 +38,66 @@ class WsConnection:
     def __init__(self, ws: WebSocket, mngr: WebsocketManager) -> None:
         self.ws = ws
         self.mngr = mngr
-        self.drone: Drone
-    
+        self.drone: Drone = None # type: ignore
+        self.pathcalculation = PathCalculation
+
     async def connect(self):
         pass
 
+    async def disconnect(self, reason: str):
+        if self.drone:
+            await self.drone.stopstream()
+        if self.drone:
+            await self.drone.disconnect()
+
+        await self.trysend(DroneDisconnected(reason=reason))
+
     async def send(self, data: ClientBoundMessage):
         await self.mngr.send(self.ws, data)
+    
+    async def trysend(self, data: ClientBoundMessage):
+        try:
+            await self.send(data)
+        except Exception:
+            pass
+
+    async def sendpathpoints(self):
+        try:
+            await self.send(self.pathcalculation.canvas_waypoints.getwaypoints())
+        except Exception:
+            pass
+
+    async def on_frame(self, data: bytes):
+        await self.mngr.send_bytes(self.ws, data)
+    
+    async def on_state(self, state: State):
+        await self.pathcalculation.incoming_callback(state=state)
+        await self.send(StateMessage(state=state))
 
     async def on_message(self, data: messages):
         session = SessionLocal()
+
         try:
             match data:
                 case ConnectToDrone():
                     self.drone = Drone(data.ip)
                     await self.drone.connect()
+                    await self.drone.startstream(self.on_frame)
+                    self.drone.set_state_callback(self.on_state)
                     await self.send(DroneConnected())
                 case TakeOff():
                     await self.drone.takeoff()
+                    await self.send(Accepted())
                 case Land():
                     await self.drone.land()
+                    await self.send(Accepted())
                 case FunkiMessage():
                     self.drone.rc(data.roll, data.pitch, data.throttle, data.yaw)
-        except TimeoutError:
-            await self.send(DroneDisconnected())
+                case DisconnectFromDrone():
+                    await self.disconnect(reason="User")
+        except TimeoutError as e:
+            await self.disconnect(reason=" ".join(e.args))
+        except BaseException as e:
+            await self.send(Error(context=e.args))
         finally:
             session.close()
