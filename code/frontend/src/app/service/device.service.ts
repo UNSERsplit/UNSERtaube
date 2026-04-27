5e5a05a6d3a65fdd9a03956e9dd76c8fedb83bc9
Noah <82201000+Nopro263@users.noreply.github.com>
2026-04-27 08:53:30 +0200

    fix

--- code/frontend/src/app/service/device.service.ts
+++ code/frontend/src/app/service/device.service.ts
@@ -2,11 +2,11 @@ import { Injectable, signal } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class DeviceService {
-    isMobile = signal(window.innerWidth <= 932);
+    isMobile = signal(window.innerWidth > 932);
 
     constructor() {
         window.addEventListener('resize', () => {
-            this.isMobile.set(window.innerWidth <= 932);
+            this.isMobile.set(window.innerWidth > 932);
         });
     }
 }
